4b79cd04d0ade6a52b5270303a74106e1aeff5be
gygachad <metal6690@gmail.com>
2022-03-22 10:49:50 -0700

    add matrix for 1

--- main.cpp
+++ main.cpp
@@ -12,91 +12,90 @@
 
 int main()
 {    
-    
-    matrix<int, 2, -1> mt0;
-    assert(mt0.size() == 0);
-    auto a = mt0[0][0];
-    assert(a == -1);
-    assert(mt0.size() == 0);
-    mt0[100][100] = 314;
-    assert(mt0[100][100] == 314);
-    assert(mt0.size() == 1);
-    /*
-    for (auto c : mt0)
+    while (1)
     {
-        int x;
-        int y;
-        int v;
-        std::tie(x, y, v) = c;
-        std::cout << x << y << v << std::endl;
+        matrix<int, 2, -1> mt0;
+        assert(mt0.size() == 0);
+        auto a = mt0[0][0];
+        assert(a == -1);
+        assert(mt0.size() == 0);
+        mt0[100][100] = 314;
+        assert(mt0[100][100] == 314);
+        assert(mt0.size() == 1);
+        /*
+        for (auto c : mt0)
+        {
+            int x;
+            int y;
+            int v;
+            std::tie(x, y, v) = c;
+            std::cout << x << y << v << std::endl;
+        }
+        */
+        matrix<int, 1, -1> mt1;
+        mt1[6] = 6;
+        assert(mt1[0] == -1);
+        assert(mt1[6] == 6);
+        assert(mt1[0] == -1);
+        mt1[5] = 5;
+        assert(mt1[2] == -1);
+        assert(mt1[5] == 5);
+
+        mt1[6] = -1;
+        assert(mt1[6] != 6);
+        assert(mt1[6] == -1);
+
+        matrix<int, 2, -1> mt2;
+        mt2[5][6] = 56;
+        assert(mt2[0][0] == -1);
+        assert(mt2[1][0] == -1);
+        assert(mt2[5][7] == -1);
+        assert(mt2[5][6] == 56);
+        mt2[5][7] = 57;
+        assert(mt2[5][6] == 56);
+        assert(mt2[6][6] == -1);
+        assert(mt2[5][7] == 57);
+        assert(mt2[1][0] == -1);
+        mt2[5][6] = -1;
+        assert(mt2[5][6] == -1);
+        assert(mt2[5][7] == 57);
+        mt2[5][7] = -1;
+        assert(mt2[5][7] != 57);
+        assert(mt2[5][7] == -1);
+        ((mt2[6][7] = 67) = -1) = 200;
+        assert(mt2[6][7] == 200);
+        assert(mt2.size() == 1);
+
+        matrix<int, 3, -1> mt3;
+        mt3[5][6][7] = 567;
+        assert(mt3[5][6][7] == 567);
+        assert(mt3[6][6][7] == -1);
+        assert(mt3[5][5][7] == -1);
+        assert(mt3[5][6][8] == -1);
+        mt3[5][6][8] = 568;
+        mt3[5][6][9] = 569;
+        mt3[6][6][7] = 667;
+        mt3[5][7][7] = 577;
+        assert(mt3.size() == 5);
+        assert(mt3[5][6][8] == 568);
+        assert(mt3[5][6][9] == 569);
+        assert(mt3[0][0][0] == -1);
+        assert(mt3[6][6][7] == 667);
+        assert(mt3[5][7][7] == 577);
+        mt3[5][7][7] = -1;
+        assert(mt3[5][7][7] == -1);
+        mt3[7][6][7] = -1;
+        assert(mt3[7][6][7] == -1);
+        mt3[6][6][7] = -1;
+        mt3[5][6][7] = -1;
+        mt3[5][6][8] = -1;
+        mt3[5][6][9] = -1;
+        assert(mt3.size() == 0);
+        assert(mt3[5][6][7] == -1);
+        assert(mt3[5][6][8] == -1);
+        assert(mt3[5][6][9] == -1);
+        assert(mt3[5][6][9] == -1);
     }
-    */
-    matrix<int, 1, -1> mt1;
-    mt1[6] = 6;
-    assert(mt1[0] == -1);
-    assert(mt1[6] == 6);
-    assert(mt1[0] == -1);
-    mt1[5] = 5;
-    assert(mt1[2] == -1);
-    assert(mt1[5] == 5);
-
-    mt1[6] = -1;
-    assert(mt1[6] != 6);
-    assert(mt1[6] == -1);
-    
-    matrix<int, 2, -1> mt2;
-    mt2[5][6] = 56;
-    assert(mt2[0][0] == -1);
-    assert(mt2[1][0] == -1);
-    assert(mt2[5][7] == -1);
-    assert(mt2[5][6] == 56);
-    mt2[5][7] = 57;
-    assert(mt2[5][6] == 56);
-    assert(mt2[6][6] == -1);
-    assert(mt2[5][7] == 57);
-    assert(mt2[1][0] == -1);
-    mt2[5][6] = -1;
-    assert(mt2[5][6] == -1);
-    assert(mt2[5][7] == 57);
-    mt2[5][7] = -1;
-    assert(mt2[5][7] != 57);
-    assert(mt2[5][7] == -1);
-
-    ((mt2[6][7] = 67) = -1) = 200;
-
-    assert(mt2[6][7] == 200);
-
-    matrix<int, 3, -1> mt3;
-    mt3[5][6][7] = 567;
-    assert(mt3[5][6][7] == 567);
-    assert(mt3[6][6][7] == -1);
-    assert(mt3[5][5][7] == -1);
-    assert(mt3[5][6][8] == -1);
-    mt3[5][6][8] = 568;
-    mt3[5][6][9] = 569;
-    mt3[6][6][7] = 667;
-    mt3[5][7][7] = 577;
-
-    mt3.size();
-
-    assert(mt3[5][6][8] == 568);
-    assert(mt3[5][6][9] == 569);
-    assert(mt3[0][0][0] == -1);
-    assert(mt3[6][6][7] == 667);
-    assert(mt3[5][7][7] == 577);
-    mt3[5][7][7] = -1;
-    assert(mt3[5][7][7] == -1);
-    mt3[7][6][7] = -1;
-    assert(mt3[7][6][7] == -1);
-
-    mt3[5][6][7] = -1;
-    mt3[5][6][8] = -1;
-    mt3[5][6][9] = -1;
-    
-    assert(mt3[5][6][7] == -1);
-    assert(mt3[5][6][8] == -1);
-    assert(mt3[5][6][9] == -1);
-    assert(mt3[5][6][9] == -1);
 
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,37 +1,94 @@
 #pragma once
 
-#include <map>
-
 template<   typename T,
             size_t matrix_size,
             T defaultValue>
-class matrix //: public std::map<int, std::conditional_t<matrix_size == 1, T, matrix<T, matrix_size - 1, defaultValue>>>
+class matrix
 {
-    typedef std::conditional_t<matrix_size == 1, T, matrix<T, matrix_size - 1, defaultValue>> mT;
-
-    //using _Mybase = std::map<int, mT>;
+    typedef matrix<T, matrix_size - 1, defaultValue> mT;
 
     std::map<int, mT> m_matrixMap;
 
     int m_lazy_key;
 
 public:
-    mT m_lazyMatrix;
-
-    matrix() { reset_lazy_value(m_lazyMatrix); }
-
-    void reset_lazy_value(T& value) { value = defaultValue; }
-
-    void reset_lazy_value(matrix<T, matrix_size - 1, defaultValue>& matrix) { matrix = mT(); }
+    mT m_lazyMatrix = mT();
   
-    bool update_lazy_matrix(T& value)
+    bool update_lazy_matrix()
     {
         bool status = false;
 
-        if (value != defaultValue)
+        if (m_lazyMatrix.update_lazy_matrix())
         {
-            m_matrixMap[m_lazy_key] = value;
-            reset_lazy_value(value);
+            m_matrixMap[m_lazy_key] = m_lazyMatrix;
+            m_lazyMatrix = mT();
+            status = true;
+        }
+        else
+        {
+            if (m_matrixMap.contains(m_lazy_key))
+            {
+                if (m_matrixMap[m_lazy_key].size() == 0)
+                {
+                    m_matrixMap.erase(m_lazy_key);
+                }
+            }
+        }
+
+        return status;
+    }
+
+    mT& operator[](int key)
+    {
+        update_lazy_matrix();
+        m_lazy_key = key;
+
+        if (m_matrixMap.contains(key))
+        {
+            return m_matrixMap[key];
+        }
+
+        return m_lazyMatrix;
+    }
+
+    size_t size()
+    {
+        update_lazy_matrix();
+
+        int key = 0;
+        mT child;
+        size_t size = 0;
+
+        for (auto c : m_matrixMap)
+        {
+            std::tie(key, child) = c;
+
+            size += child.size();
+        }
+
+        return size;
+    }
+};
+
+template<   typename T,
+            T defaultValue>
+class matrix<T, 1, defaultValue>
+{
+    std::map<int, T> m_matrixMap;
+
+    int m_lazy_key;
+
+public:
+    T m_lazyMatrix = defaultValue;
+
+    bool update_lazy_matrix()
+    {
+        bool status = false;
+
+        if (m_lazyMatrix != defaultValue)
+        {
+            m_matrixMap[m_lazy_key] = m_lazyMatrix;
+            m_lazyMatrix = defaultValue;
             status = true;
         }
         else
@@ -45,35 +102,10 @@ public:
 
         return status;
     }
-   
-    bool update_lazy_matrix(matrix<T, matrix_size - 1, defaultValue>& matrix)
+
+    T& operator[](int key)
     {
-        bool status = false;
-
-        //make cycle for???
-        if (matrix.update_lazy_matrix(matrix.m_lazyMatrix))
-        {
-            m_matrixMap[m_lazy_key] = matrix;
-            reset_lazy_value(matrix);
-            status = true;
-        }
-        else
-        {
-            if (m_matrixMap.contains(m_lazy_key))
-            {
-                if (m_matrixMap[m_lazy_key].child_size() == 0)
-                {
-                    m_matrixMap.erase(m_lazy_key);
-                }
-            }
-        }
-
-        return status;
-    }
-
-    mT& operator[](int key)
-    {
-        update_lazy_matrix(m_lazyMatrix);
+        update_lazy_matrix();
         m_lazy_key = key;
 
         if (m_matrixMap.contains(key))
@@ -84,34 +116,10 @@ public:
         return m_lazyMatrix;
     }
 
-    //For type matrix
-    size_t child_size()
-    {
-        return m_matrixMap.size();
-    }
-
-    //For type T
-    /*
-    size_t child_size()
-    {
-
-    }
-    */
     size_t size()
     {
-        update_lazy_matrix(m_lazyMatrix);
+        update_lazy_matrix();
 
-        int key = 0;
-        mT child;
-        size_t size = 0;
-
-        for (auto c : m_matrixMap)
-        {
-            std::tie(key, child) = c;
-
-            size += child.child_size();
-        }
-
-        return size;
+        return m_matrixMap.size();
     }
 };
